8e650493e35344d26727c50cbf5785be760b2f8e
sudilakuruppu <sudilakuruppu@gmail.com>
2026-03-17 09:59:45 +0000

    Initial commit - add programming fundamentals files

---
+++ 40695/day-02/main.py
@@ -0,0 +1,38 @@
+## Question 01
+print("Sudila Sandamith")
+print("Royal College")
+
+## Question 02
+result = 22 / 7
+print("22 devided by 7 is:", result)
+
+## Question 03
+operation = "sum"
+total = 8
+print("The" + operation + " total is: " + str(total))
+
+## Question 04
+length = 10
+width = 5
+area = length * width
+print("The area of the rectangle with length " + str(length) + "and width " + str(width) + " is: " + str(area))
+
+## Question 05
+name = input("Enter your name: ")
+print("Welcome, " + name + "! Glad to have you here.")
+print("Data type of 'name' is:", type(name))
+
+## Question 06
+birth_year = input("Enter your birth year: ")
+birth_year_int = int(birth_year)
+
+print("Your birth year is: " + str(birth_year_int))
+print("Data type after typecasting:", type(birth_year_int))
+
+## Question 07
+CURRENT_YEAR = 2026
+
+birth_year = int(input("Enter your birth year: "))
+age = CURRENT_YEAR - birth_year
+
+print("Your age is: " + str(age))

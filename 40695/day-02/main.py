b794601636a69208720676bff8d6e77a2ef3cd92
sudilakuruppu <sudilakuruppu@gmail.com>
2026-03-17 10:18:25 +0000

    Fix syntax errors and typos in main.py

--- 40695/day-02/main.py
+++ 40695/day-02/main.py
@@ -1,38 +1,38 @@
-## Question 01
+# Question 01
 print("Sudila Sandamith")
 print("Royal College")
 
-## Question 02
+# Question 02
 result = 22 / 7
-print("22 devided by 7 is:", result)
+print("22 divided by 7 is:", result)
 
-## Question 03
+# Question 03
 operation = "sum"
 total = 8
-print("The" + operation + " total is: " + str(total))
+print("The " + operation + " total is: " + str(total))
 
-## Question 04
+# Question 04
 length = 10
 width = 5
 area = length * width
-print("The area of the rectangle with length " + str(length) + "and width " + str(width) + " is: " + str(area))
+print("The area of the rectangle with length " + str(length) + " and width " + str(width) + " is: " + str(area))
 
-## Question 05
-name = input("Enter your name: ")
+# Question 05
+name = "Sudila"
 print("Welcome, " + name + "! Glad to have you here.")
 print("Data type of 'name' is:", type(name))
 
-## Question 06
-birth_year = input("Enter your birth year: ")
+# Question 06
+birth_year = "2000"
 birth_year_int = int(birth_year)
 
 print("Your birth year is: " + str(birth_year_int))
-print("Data type after typecasting:", type(birth_year_int))
+print(type(birth_year_int))
 
-## Question 07
+# Question 07
 CURRENT_YEAR = 2026
 
-birth_year = int(input("Enter your birth year: "))
+birth_year = 2000
 age = CURRENT_YEAR - birth_year
 
 print("Your age is: " + str(age))
